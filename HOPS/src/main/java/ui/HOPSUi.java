285d245ffb83f7c44447c2eb455cef39652ed19d
Timo Reijonen <timo.reijonen@helsinki.fi>
2019-04-02 15:36:16 +0300

    Kolmannen viikon muutokset

--- HOPS/src/main/java/ui/HOPSUi.java
+++ HOPS/src/main/java/ui/HOPSUi.java
@@ -8,6 +8,8 @@ package ui;
 import dao.*;
 import database.Database;
 import domain.*;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  *
@@ -16,19 +18,75 @@ import domain.*;
 public class HOPSUi {
 
     private HOPSService HOPSService;
-    
+    private Scanner reader;
+
     public HOPSUi() {
-        Database database = new Database("jdbc:sqlite:tietokanta.db");
+        Database database = new Database("jdbc:sqlite:HOPSDatabase.db");
         SQLStudentDao sDao = new SQLStudentDao(database);
         SQLCourseDao cDao = new SQLCourseDao(database);
         this.HOPSService = new HOPSService(sDao, cDao);
+        this.reader = new Scanner(System.in);
     }
 
-    public static void main(String[] args) {
-        HOPSUi ui = new HOPSUi();
+    public void start() throws SQLException {
         System.out.println("Keskeneräinen versio HOPS-ohjelmasta.");
-        System.out.println("Ainoa tuettu ominaisuus on uuden opiskelijan luonti.");
-        System.out.print("Järjestelmässä olevat opiskelijat: ");
-        
+        System.out.println("Ainoat tuetut ominaisuudet ovat uuden opiskelijan luonti ja opiskelijoiden tulostus.");
+        OUTER:
+        while (true) {
+            printOptions();
+            System.out.print("Mitä haluat tehdä? ");
+            int order = readUserInput();
+            System.out.println("");
+            switch (order) {
+                case 1:
+                    printStudents();
+                    break;
+                case 2:
+                    saveNewStudent();
+                    break;
+                case 3:
+                    System.out.println("Hyvää päivänjatkoa!");
+                    break OUTER;
+                default:
+                    System.out.println("Ei kelpo komento.");
+                    break;
+            }
+            System.out.println("");
+        }
+    }
+
+    public void printStudents() throws SQLException {
+        System.out.println("Järjestelmässä olevat opiskelijat: ");
+        HOPSService.printAllStudents();
+    }
+
+    public void saveNewStudent() throws SQLException {
+        System.out.println("Lisätään uusi opiskelija: ");
+        System.out.print("Nimi: ");
+        String name = this.reader.nextLine();
+        System.out.print("Käyttäjänimi: ");
+        String uName = reader.nextLine();
+        boolean created = HOPSService.createNewUser(name, uName);
+        if (!created) {
+            System.out.println("Käyttäjätunnuksen täytyy olla uniikki.");
+        } else {
+            System.out.println("Uusi opiskelija lisätty");
+        }
+    }
+
+    public void printOptions() {
+        System.out.println("1: tulosta opiskelijat.");
+        System.out.println("2: lisää uusi opiskelija.");
+        System.out.println("3: poistu ohjelmasta.");
+    }
+
+    public int readUserInput() {
+        int order = Integer.parseInt(this.reader.nextLine());
+        return order;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        HOPSUi ui = new HOPSUi();
+        ui.start();
     }
 }
--- dokumentointi/tyoaikakirjanpito.md
+++ dokumentointi/tyoaikakirjanpito.md
@@ -6,3 +6,5 @@ Päivä | Tunnit | Mitä tein
 27.3. | 2h | sekvenssikaavioiden ja luokkakaavioiden kertaus
 28.3. | 2h | domain-luokat ja kurssimateriaalin opiskelu
 29.3. | 3h | DAO:n kertaus ja työn edistys
+1.4. | 2h | koodausta
+2.4. | 3,5h | sovelluksen viimeistely viikkopalautusta varten ja muuta säätöä
